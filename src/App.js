ed5fa76f595fdddb65003c5b8d34d45795b76717
moodmister <v.stoyanov0069@gmail.com>
2023-05-12 13:42:08 +0300

    Add game logic and winning sequence

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 let xTurn = true;
+let isGameFinished = false;
 
 function Square({id, buttonText, handleSquareClick}) {
   return <button id={id} className="square" onClick={ev => { handleSquareClick(Number(ev.target.id)); }}>{buttonText}</button>;
@@ -18,23 +19,109 @@ export default function Board() {
   const [dimension, setDimension] = useState(3);
   const [squares, setSquares] = useState(Array(dimension * dimension).fill(''));
   const [winCondition, setWinCondition] = useState(3);
-  const [winner, setWinner] = useState('No winner');
+  const [winner, setWinner] = useState('Play!');
 
-  function checkWinner() {
-    // TODO Add logic for winning
-    return 'No winner';
+  function checkBoardVertically(playedSymbol, newSquares) {
+    for (let i = 0; i < dimension * dimension; i++) {
+      for (let j = 0; j < winCondition; j++) {
+        if (newSquares[i] !== playedSymbol) {
+          break;
+        }
+        if (j === winCondition - 1) {
+          return true;
+        }
+        i = i + dimension;
+      }
+    }
+    return false;
+  }
+  function checkBoardHorizontally(playedSymbol, newSquares) {
+    for (let i = 0; i < dimension * dimension; i++) {
+      for (let j = 0; j < winCondition; j++) {
+        if (newSquares[i] !== playedSymbol) {
+          break;
+        }
+        if (j === winCondition - 1) {
+          return true;
+        }
+        if (i === dimension - 1 && j < winCondition - 1) {
+          break;
+        }
+        i = i + 1;
+      }
+    }
+    return false;
+  }
+
+  function checkBoardMainDiagonal(playedSymbol, newSquares) {
+    for (let i = 0; i < dimension * dimension; i++) {
+      for (let j = 0; j < winCondition; j++) {
+        if (newSquares[i] !== playedSymbol) {
+          break;
+        }
+        if (j === winCondition - 1) {
+          return true;
+        }
+        if ((i + 1) % dimension === 0 && j < winCondition - 1) {
+          break;
+        }
+        if (i >= dimension * dimension - dimension && i < dimension * dimension && j < winCondition - 1) {
+          break;
+        }
+        i = i + dimension + 1;
+      }
+    }
+    return false;
+  }
+
+  function checkBoardOppositeDiagonal(playedSymbol, newSquares) {
+    for (let i = 0; i < dimension * dimension; i++) {
+      for (let j = 0; j < winCondition; j++) {
+        if (newSquares[i] !== playedSymbol) {
+          break;
+        }
+        if (j === winCondition - 1) {
+          return true;
+        }
+        if (i % dimension === 0 && j < winCondition - 1) {
+          break;
+        }
+        if (i >= dimension * dimension - dimension && i < dimension * dimension && j < winCondition - 1) {
+          break;
+        }
+        i = i + dimension - 1;
+      }
+    }
+    return false;
+  }
+
+  function checkWinner(newSquares) {
+    const playedSymbol = xTurn ? 'X' : 'O';
+
+    if (checkBoardVertically(playedSymbol, newSquares) ||
+      checkBoardHorizontally(playedSymbol, newSquares) ||
+      checkBoardMainDiagonal(playedSymbol, newSquares) ||
+      checkBoardOppositeDiagonal(playedSymbol, newSquares)) {
+      isGameFinished = true;
+      document.body.classList.add('rave');
+        return `${playedSymbol} wins`;
+      }
+    return 'Play!';
   }
 
   function handleSquareClick(idx) {
     if (!!squares[idx]) {
       return;
     }
+    if (isGameFinished) {
+      return;
+    }
     const newSquares = [...squares];
     newSquares[idx] = xTurn ? 'X' : 'O';
-    xTurn = !xTurn;
-    const winner = checkWinner();
-    setWinner(winner);
     setSquares(newSquares);
+    const winner = checkWinner(newSquares);
+    xTurn = !xTurn;
+    setWinner(winner);
   }
 
   const rows = [];
@@ -42,23 +129,42 @@ export default function Board() {
     rows.push(<Row key={i} squareKey={i} dimension={dimension} squareText={squares} handleSquareClick={handleSquareClick}></Row>);
   }
 
-  function clearBoard() {
+  function resetGame() {
+    isGameFinished = false;
+    xTurn = true;
+    setWinner("Play!");
+    document.body.classList.remove('rave');
     setSquares(Array(dimension * dimension).fill(''));
+  }
+
+  function changeDimension(dimension) {
+    if (dimension >= 3 && dimension <= 16) {
+      setDimension(Number(dimension));
+    }
+    resetGame();
+  }
+
+  function changeWinCondition(condition) {
+    if (condition >= 2 && condition <= dimension) {
+      setWinCondition(Number(condition));
+    }
+    resetGame();
   }
 
   return (
     <div className="game">
       <div className="controls">
+        <strong>Warning! Contains flashy images!</strong>
         <div className="game-info">{winner}</div>
         <label>
           Board dimension
-          <input type="number" style={{ width: '40px' }} value={dimension} min={3} max={16} onInput={event => { setDimension(event.target.value * 1); clearBoard(); }}></input><br></br>
+          <input type="number" style={{ width: '40px' }} value={dimension} min={3} max={16} onInput={event => { changeDimension(event.target.value); }}></input><br></br>
         </label>
         <label>
           Win condition
-          <input type="number" style={{ width: '40px' }} value={winCondition} min={3} max={5} onInput={event => { setWinCondition(event.target.value * 1); clearBoard(); }}></input><br></br>
+          <input type="number" style={{ width: '40px' }} value={winCondition} min={3} max={dimension} onInput={event => { changeWinCondition(event.target.value); }}></input><br></br>
         </label>
-        <button onClick={() => { clearBoard(); }}>Reset</button>
+        <button onClick={() => { resetGame(); }}>Reset</button>
       </div>
       <div className="board">
         {rows}
